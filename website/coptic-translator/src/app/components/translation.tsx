39e3cf26d2f01540c2a7b60d02f36909fc8af604
Andrew Megalaa <andrewmuhareb@gmail.com>
2023-12-12 22:40:58 -0500

    Co-authored-by: Maxim Enis <enismaxim1@users.noreply.github.com>

--- website/coptic-translator/src/app/components/translation.tsx
+++ website/coptic-translator/src/app/components/translation.tsx
@@ -1,87 +1,78 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, use, useEffect } from "react";
 import { FaExchangeAlt } from "react-icons/fa";
 // import css
 import "./translation.css";
 
+const ENGLISH_API = "/api/predictions/my_tc";
+const COPTIC_API = "/api/predictions/my_tc";
+
 const TranslationComponent: React.FC = () => {
-  const [englishText, setEnglishText] = useState<string>("");
-  const [copticText, setCopticText] = useState<string>("");
+  const [srcText, setSrcText] = useState<string>("");
+  const [tgtText, setTgtText] = useState<string>("");
+  const [tgtTextLoading, setTgtTextLoading] = useState<boolean>(false);
   const [isEnglishToCoptic, setIsEnglishToCoptic] = useState<boolean>(true);
-  const [leftBoxHeader, setLeftBoxHeader] = useState<string>("English");
-  const [rightBoxHeader, setRightBoxHeader] = useState<string>("Coptic");
 
-  const placeholderText = (language: string) => {
-    return `Type or paste ${language} text here...`;
+  const placeholderText = (isEnglishToCoptic: boolean) => {
+    return `Type or paste ${
+      isEnglishToCoptic ? "English" : "Coptic"
+    } text here...`;
   };
 
-  const getEnglishTranslation = (word: string) => {
-    return "english word";
-  };
-
-  const getCopticTranslation = (word: string) => {
-    return "coptic word";
-  };
-
-  const translateText = (isEnglishToCoptic: boolean) => {
+  useEffect(() => {
+    let api = ENGLISH_API;
     if (isEnglishToCoptic) {
-      setCopticText(getCopticTranslation(englishText));
-    } else {
-      setEnglishText(getEnglishTranslation(copticText));
+      api = COPTIC_API;
     }
-  };
+    if (srcText === "") {
+      return;
+    }
+    setTgtTextLoading(true);
+    fetch(api, {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: srcText,
+    })
+      .then((response) => response.text())
+      .then((response) => {
+        setTgtText(response);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setTgtTextLoading(false));
+  }, [srcText, isEnglishToCoptic]);
 
-  const handleTranslateClick = () => {
-    translateText(isEnglishToCoptic);
-  };
-
-  const handleReverseTranslation = () => {
-    setIsEnglishToCoptic(!isEnglishToCoptic);
-    setBoxHeaders(!isEnglishToCoptic);
-    // setEnglishText(isEnglishToCoptic ? copticText : "");
-    // setCopticText(isEnglishToCoptic ? "" : englishText);
-    translateText(!isEnglishToCoptic);
-  };
-
-  const setBoxHeaders = (isEnglishToCoptic: boolean) => {
-    setLeftBoxHeader(isEnglishToCoptic ? "English" : "Coptic");
-    setRightBoxHeader(isEnglishToCoptic ? "Coptic" : "English");
-  };
-
-  const handleEnglishChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const inputText: string = e.target.value;
-    setEnglishText(inputText);
-  };
-
-  const handleCopticChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const inputText: string = e.target.value;
-    setCopticText(inputText);
-  };
+  useEffect(() => {
+    const tempText = srcText;
+    setSrcText(tgtText);
+    setTgtText(tempText);
+  }, [isEnglishToCoptic]);
 
   return (
     <div className="flex flex-col items-center bg-egyptian min-h-screen p-8 text-teal">
       <div className="w-full flex justify-center text-scriptorium-red">
         <div className="w-1/2 pr-4">
           <h2 className="text-2xl mb-4 text-center font-hieroglyph">
-            {leftBoxHeader}
+            {isEnglishToCoptic ? "English" : "Coptic"}
           </h2>
           <textarea
             className="border p-2 w-full h-40 bg-scriptorium-red-left text-teal rounded-lg"
-            value={isEnglishToCoptic ? englishText : copticText}
-            onChange={isEnglishToCoptic ? handleEnglishChange : handleCopticChange}
-            placeholder={placeholderText(leftBoxHeader)}
-            // set outline color 
-            style={{outline: "none"}}
+            onChange={(e) => setSrcText(e.target.value)}
+            value={srcText}
+            placeholder={placeholderText(isEnglishToCoptic)}
+            // set outline color
+            style={{ outline: "none" }}
           />
         </div>
         <div className="flex flex-col items-center justify-center">
           <button
-            onClick={handleTranslateClick}
+            // onClick={handleTranslateClick}
             className="p-2 bg-scriptorium-red-left text-teal rounded-pyramid shadow-md mb-4 font-hieroglyph"
           >
             Translate
           </button>
           <button
-            onClick={handleReverseTranslation}
+            onClick={() => setIsEnglishToCoptic(!isEnglishToCoptic)}
             className="p-2 bg-scriptorium-red-left text-teal rounded-pyramid shadow-md"
           >
             <FaExchangeAlt size={24} />
@@ -89,19 +80,18 @@ const TranslationComponent: React.FC = () => {
         </div>
         <div className="w-1/2 pl-4">
           <h2 className="text-2xl mb-4 text-center text-scriptorium-red">
-            {rightBoxHeader}
+            {isEnglishToCoptic ? "Coptic" : "English"}
           </h2>
           <textarea
             className="border p-2 w-full h-40 bg-scriptorium-red-right text-teal rounded-lg no-highlights"
-            value={isEnglishToCoptic ? copticText : englishText}
-            readOnly={true}  
-            style={{outline: "none"}} 
+            value={tgtTextLoading ? tgtText + "..." : tgtText}
+            readOnly={true}
+            style={{ outline: "none" }}
           />
         </div>
       </div>
     </div>
   );
-  
 };
 
 export default TranslationComponent;
