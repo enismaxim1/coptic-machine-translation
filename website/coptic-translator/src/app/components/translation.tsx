534b3f3b13539afd5923600b7e06959ea4d13ac8
Andrew Megalaa <andrewmuhareb@gmail.com>
2023-12-13 23:47:09 -0500

    improved UI

--- website/coptic-translator/src/app/components/translation.tsx
+++ website/coptic-translator/src/app/components/translation.tsx
@@ -3,14 +3,19 @@ import { FaExchangeAlt } from "react-icons/fa";
 // import css
 import "./translation.css";
 
-const ENGLISH_API = "/api/predictions/my_tc";
-const COPTIC_API = "/api/predictions/my_tc";
+const ENGLISH_API = "/api/predictions/english";
+const COPTIC_API = "/api/predictions/coptic";
+const DELAY = 500;
+const regexEnglish = /^[a-zA-Z\s.,!?'"-]*$/;
+const regexCoptic = /^[\u2C80-\u2CFF\u03E2-\u03EF\s.,!?'"-]*$/;
 
 const TranslationComponent: React.FC = () => {
   const [srcText, setSrcText] = useState<string>("");
   const [tgtText, setTgtText] = useState<string>("");
   const [tgtTextLoading, setTgtTextLoading] = useState<boolean>(false);
   const [isEnglishToCoptic, setIsEnglishToCoptic] = useState<boolean>(true);
+  const srcTextRef = React.useRef<HTMLTextAreaElement>(null);
+  const tgtTextRef = React.useRef<HTMLTextAreaElement>(null);
 
   const placeholderText = (isEnglishToCoptic: boolean) => {
     return `Type or paste ${
@@ -19,27 +24,40 @@ const TranslationComponent: React.FC = () => {
   };
 
   useEffect(() => {
-    let api = ENGLISH_API;
-    if (isEnglishToCoptic) {
-      api = COPTIC_API;
-    }
-    if (srcText === "") {
-      return;
-    }
-    setTgtTextLoading(true);
-    fetch(api, {
-      method: "POST",
-      headers: {
-        "Content-Type": "text/plain",
-      },
-      body: srcText,
-    })
-      .then((response) => response.text())
-      .then((response) => {
-        setTgtText(response);
+    let translationTimeout: NodeJS.Timeout;
+    const controller = new AbortController();
+    const signal = controller.signal;
+    translationTimeout = setTimeout(() => {
+      const api = isEnglishToCoptic ? COPTIC_API : ENGLISH_API;
+      if (srcText === "") {
+        setTgtText("");
+        return;
+      }
+      setTgtTextLoading(true);
+
+      fetch(api, {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+        },
+        body: srcText,
+        signal: signal,
       })
-      .catch((error) => console.error(error))
-      .finally(() => setTgtTextLoading(false));
+        .then((response) => response.json())
+        .then((response) => {
+          if (!response.translation) {
+            throw new Error("Translation failed");
+          }
+          setTgtText(response.translation);
+        })
+        .catch((error) => console.error(error))
+        .finally(() => setTgtTextLoading(false));
+    }, DELAY);
+    return () => {
+      clearTimeout(translationTimeout);
+      controller.abort();
+      setTgtTextLoading(false);
+    };
   }, [srcText, isEnglishToCoptic]);
 
   useEffect(() => {
@@ -47,6 +65,26 @@ const TranslationComponent: React.FC = () => {
     setSrcText(tgtText);
     setTgtText(tempText);
   }, [isEnglishToCoptic]);
+
+  const handleSrcTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (isEnglishToCoptic) {
+      if (regexEnglish.test(newText)) {
+        setSrcText(newText);
+      }
+    } else {
+      if (regexCoptic.test(newText)) {
+        setSrcText(newText);
+      }
+    }
+  };
+
+  const autoResize = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (srcTextRef.current && tgtTextRef.current) {
+      srcTextRef.current.style.height = `${e.target.scrollHeight}px`;
+      tgtTextRef.current.style.height = `${e.target.scrollHeight}px`;
+    }
+  };
 
   return (
     <div className="flex flex-col items-center bg-egyptian min-h-screen p-8 text-teal">
@@ -56,21 +94,23 @@ const TranslationComponent: React.FC = () => {
             {isEnglishToCoptic ? "English" : "Coptic"}
           </h2>
           <textarea
-            className="border p-2 w-full h-40 bg-scriptorium-red-left text-teal rounded-lg"
-            onChange={(e) => setSrcText(e.target.value)}
+            ref={srcTextRef}
+            className="border p-2 w-full bg-scriptorium-red-left text-teal rounded-lg"
+            onChange={(e) => {
+              handleSrcTextChange(e);
+              autoResize(e);
+            }}
             value={srcText}
             placeholder={placeholderText(isEnglishToCoptic)}
-            // set outline color
-            style={{ outline: "none" }}
+            style={{
+              minHeight: "10rem",
+              resize: "none",
+              overflow: "hidden",
+              outline: "none",
+            }}
           />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <button
-            // onClick={handleTranslateClick}
-            className="p-2 bg-scriptorium-red-left text-teal rounded-pyramid shadow-md mb-4 font-hieroglyph"
-          >
-            Translate
-          </button>
           <button
             onClick={() => setIsEnglishToCoptic(!isEnglishToCoptic)}
             className="p-2 bg-scriptorium-red-left text-teal rounded-pyramid shadow-md"
@@ -83,10 +123,16 @@ const TranslationComponent: React.FC = () => {
             {isEnglishToCoptic ? "Coptic" : "English"}
           </h2>
           <textarea
-            className="border p-2 w-full h-40 bg-scriptorium-red-right text-teal rounded-lg no-highlights"
+            ref={tgtTextRef}
+            className="border p-2 w-full bg-scriptorium-red-right text-teal rounded-lg no-highlights"
             value={tgtTextLoading ? tgtText + "..." : tgtText}
             readOnly={true}
-            style={{ outline: "none" }}
+            style={{
+              minHeight: "10rem",
+              resize: "none",
+              overflow: "hidden",
+              outline: "none",
+            }}
           />
         </div>
       </div>
